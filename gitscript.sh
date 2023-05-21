33660b2cf7787297c9831aa69c2d154c2f83ed6c
Shree Singhi <singhishree@gmail.com>
2023-05-21 17:21:52 +0530

    add autouser detect functionality

--- gitscript.sh
+++ gitscript.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-read -p "enter username: " username
-read -p "enter your super secure private secret confidential key" key
+read -s -p "enter your super secure private secret confidential key: " key
 read -p "enter repo name: " repo_name
 read -p "enter description file name: " fname
 description=$(<"$fname")
 
-
-
-mkdir $repo_name
-cd $repo_name
 git init
+
+if [ "${#username}" -eq 0 ]; then
+    read -p "enter username: " user
+    export username=$user
+fi
 
 curl -L -X POST -H "Authorization: Bearer $key" https://api.github.com/user/repos -d "{\"name\":\"$repo_name\",\"description\":\"$description\"}" > /dev/null
 
-touch README.md
+touch .gitignore
+echo gitscript.sh >> .gitignore
+echo $fname >> .gitignore
 git add -A
 git commit . -m "Initial commit"
 git branch -m master main
 
-git remote add origin "git@github.com:$username/$repo_name.git"
-git push origin main
+git push --set-upstream "https://$key@github.com/$username/$repo_name.git" main
+
 
 echo "done! good job"
