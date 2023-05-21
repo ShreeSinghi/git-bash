bc0f8a7f0ab7bdc233ccb521e9e2bd086820877c
Shree Singhi <singhishree@gmail.com>
2023-05-21 23:35:25 +0530

    check if repo already exists

--- gitscript.sh
+++ gitscript.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+ask_repo() {
+  read -p "enter repo name: " repo_name
+  # tries to match git.....yay
+  while ! [[ "$repo_name" =~ ^git.*yay$ ]]
+  do
+    echo "invalid format"
+    read -p "enter repo name: " repo_name
+  done
+}
+
 read -s -p "enter your super secure private secret confidential key: " key
+echo ""
 
 read -p "enter description file name: " fname
 description=$(<"$fname")
 
-
-read -p "enter repo name: " repo_name
-# tries to match git.....yay
-while ! [[ "$repo_name" =~ ^git.*yay$ ]]
+while [[ $? -eq 1 ]]
 do
-  read -p "enter repo name: " repo_name
+  echo "file aint existing"
+  read -p "enter description file name: " fname
+  description=$(<"$fname")
 done
 
 git init
@@ -20,7 +30,16 @@ if [ "${#username}" -eq 0 ]; then
     export username=$user
 fi
 
-curl -L -X POST -H "Authorization: Bearer $key" https://api.github.com/user/repos -d "{\"name\":\"$repo_name\",\"description\":\"$description\"}" > /dev/null
+ask_repo
+
+response=$( curl -L -X POST -H "Authorization: Bearer $key" https://api.github.com/user/repos -d "{\"name\":\"$repo_name\",\"description\":\"$description\"}" )
+
+while [[ "$response" =~ ^.*message\":\ \"name\ already\ exists\ on\ this\ account.*$ ]]
+do
+  echo "Repository already exists :("
+  ask_repo
+  response=$( curl -L -X POST -H "Authorization: Bearer $key" https://api.github.com/user/repos -d "{\"name\":\"$repo_name\",\"description\":\"$description\"}" )
+done
 
 touch .gitignore
 echo gitscript.sh >> .gitignore
@@ -29,7 +48,6 @@ git add -A
 git commit . -m "Initial commit"
 git branch -m master main
 
-git push --set-upstream "https://$key@github.com/$username/$repo_name.git" main
-
+git remote add origin "https://$key@github.com/$username/$repo_name.git"
 
 echo "done! good job"
